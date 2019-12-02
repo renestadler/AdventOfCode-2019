27e91edbb21a13290072fd73fcafb9704ae5a395
renestadler <stadler.rene2000@gmail.com>
2019-12-02 07:48:26 +0100

    Solved Day2
    Created my Solution for the second day of AdventOfCode.

--- days/D02.ts
+++ days/D02.ts
@@ -1,17 +1,43 @@
 export class Main {
     public static start(input: string) {
-        let answer1 = this.part1(input.split('\r\n'));
-        let answer2 = this.part2(input.split('\r\n'));
+        let answer1 = this.part1(input.split(','));
+        let answer2 = this.part2(input.split(','));
         return [answer1, answer2];
     }
 
     public static part1(inputs: string[]) {
-        let count: number = 0;
-        return count;
+        var stack = inputs.map(f => parseInt(f));
+        let index = 0;
+        stack[1] = 12;
+        stack[2] = 2;
+        while (stack[index] <= 99) {
+            switch (stack[index]) {
+                case 1: stack[stack[index + 3]] = stack[stack[index + 1]] + stack[stack[index + 2]]; break;
+                case 2: stack[stack[index + 3]] = stack[stack[index + 1]] * stack[stack[index + 2]]; break;
+            }
+            index += 4;
+        }
+        return stack[0];
     }
 
     public static part2(inputs: string[]) {
-        let count = 0;
-        return count;
+        for (let index1 = 0; index1 < 100; index1++) {
+            for (let index2 = 0; index2 < 100; index2++) {
+                var stack = inputs.map(f => parseInt(f));
+                let index = 0;
+                stack[1] = index1;
+                stack[2] = index2;
+                while (stack[index] <= 99) {
+                    switch (stack[index]) {
+                        case 1: stack[stack[index + 3]] = stack[stack[index + 1]] + stack[stack[index + 2]]; break;
+                        case 2: stack[stack[index + 3]] = stack[stack[index + 1]] * stack[stack[index + 2]]; break;
+                    }
+                    index += 4;
+                }
+                if (stack[0] == 19690720) {
+                    return 100 * index1 + index2;
+                }
+            }
+        }
     }
 }
